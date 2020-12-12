3e50d47191e666f725b0f9f210eef5ef1fcb2569
YCLTW1995 <yl4337@columbia.edu>
2020-12-11 18:22:58 -0800

    parallel listarray

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Data.Array
 import Control.Parallel.Strategies
 import Data.Foldable(toList)
 import Control.Monad.Par
-
+import Control.DeepSeq
 
 (!?) :: Ix i => Array i e -> i -> Maybe e
 a !? j | bounds a `inRange` j = Just (a ! j)
@@ -20,9 +20,13 @@ data PArray i f = i :. Array i f
 
 instance Ix i => Comonad (PArray i) where -- possibility of parallelism here
     extract (j:.a) = a ! j
-    f `extend` (j:.a) = j :. listArray bds (fmap (f . flip (:.) a) (range bds) `using` parLIST rseq)
-        where bds = bounds a
+    
+    extend f (i :. a)  = i :. my_listArray (bounds a) (f . (:.a) <$> indices a)
+    
+    --f `extend` (j:.a) = j :. listArray bds (fmap (f . flip (:.) a) (range bds) `using` parLIST rseq)
+        --where bds = bounds a
     -- http://blog.sigfpe.com/2008/03/comonadic-arrays.html
+       -- extend :: (w a -> b ) -> w a -> w b
 
 
 parLIST:: Strategy a -> Strategy [a]
@@ -36,6 +40,20 @@ parMAP f (a:as) = do
   return (b:bs)
 
 
+my_listArray             :: (Ix a) => (a,a) -> [b] -> Array a b
+my_listArray b vs        =  array b (runEval ( zipwith_par (\ a b -> (a,b)) (range b) vs))
+
+zipwith_seq::(a -> b -> c) -> [a] -> [b] -> [c]
+zipwith_seq f (x:xs) (y:ys) = (f x y) : (zipwith_seq f xs ys)
+zipwith_seq _ _ _ = []
+
+zipwith_par :: (a -> b -> c) -> [a] -> [b] -> Eval [c]
+zipwith_par _ [] [] = return []
+zipwith_par f (a:as) (b:bs) = do
+  c <- rpar (f a b)
+  cs <- zipwith_par f as bs
+  return (c:cs) 
+
 type LocOp i f = PArray i (Maybe f) -> Maybe f
 
 -- I think these can be combined into one using the graph laplacian,
@@ -46,7 +64,7 @@ lap1 pa @ (i :. a) = do l <- join $ a !? (i - 1)
                         r <- join $ a !? (i + 1)
                         c <- extract pa
                         return $ l + r - 2 * c
-                  
+
 
 lap2 :: Num f => LocOp (Int, Int) f
 lap2 pa@((x,y):.a) = do n <- join $ a !? (x, y + 1)
@@ -68,26 +86,24 @@ boundStep :: (Ix i, Num f) => Bound i f -> LocOp i f -> LocOp i f
 boundStep b d pa@(j:._) = mplus (b ! j) $ (+) <$> extract pa <*> d pa
 
 
-simulate :: (Ix i, Num f) => LocOp i f -> PArray i (Maybe f) -> [PArray i (Maybe f)]
-simulate d pa = pa : map (extend d) (simulate d pa)
+simulate :: (Ix i, Num f) => LocOp i f -> PArray i (Maybe f)  -> [PArray i (Maybe f)]
+simulate d pa = pa : map (extend d) (simulate d pa) 
 
 
-execution :: (Ix i, Num f) =>  LocOp i f -> PArray i (Maybe f)-> [[f]]
-execution bdiff1 a1 = map (map rmJ . toList) $ simulate bdiff1 a1
+execution :: (Ix i, Num f) =>  LocOp i f -> PArray i (Maybe f)-> [[f]] 
+execution bdiff1 a1 = map (map rmJ . toList) $ simulate bdiff1 a1 
     where rmJ Nothing = undefined
           rmJ (Just n) = n
-
-
 
 main :: IO()
 main = do
   let b1 = listArray (0, 10) (Just 1 : replicate 9 Nothing ++ [Just 1])
   let bdiff1 = boundStep b1 (fmap (*0.1) . lap1)
   let a1 = 0 :. listArray (0, 10) (replicate 11 $ Just 0)
-  let l1 = execution bdiff1 a1
+  let l1 = execution bdiff1 a1 
 
-  let result = l1 !! 10000
-  print $ result
+  let result = l1 !! 12
+  print $ result 
   putStrLn "Main Function Done"
 
 -- use "stack exec -- pfp-final-exe +RTS -N2 -ls" to records event 
