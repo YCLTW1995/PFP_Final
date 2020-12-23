410e85660d9322c33fa193e50e004011a8ef195d
Morrin0 <codymorrin@gmail.com>
2020-12-22 21:50:36 -0800

    fixed parallelism

--- app/Main.hs
+++ app/Main.hs
@@ -1,49 +1,40 @@
+{-# LANGUAGE TypeOperators #-}
+
 import Lib
-
 import qualified Data.Array.Repa as R
+import System.Environment as S
 
-b1 = pad (Just 1) $ R.fromFunction (R.Z R.:. (8 :: Int)) $ const Nothing
-bdiff1 = boundStep b1 (R.map (*0.1) . lap')
-a1 = R.fromFunction (R.Z R.:. (10 :: Int)) $ const 0
-l1 = simulate bdiff1 a1
+line :: Int -> System R.U (R.Z R.:. Int) Double
+line n = System iv bounds ham where
+  iv = R.fromFunction (R.Z R.:. (n + 2)) $ const 0
+  bounds = pad (Just 1) $ R.fromFunction (R.Z R.:. n) $ const Nothing
+  ham = pad (0 :: Double) . R.map (*0.1) . lap
 
+square :: Int -> System R.U (R.Z R.:. Int R.:. Int) Double
+square n = System iv bounds ham where
+  iv = R.fromFunction (R.Z R.:. (n + 2) R.:. (n + 2)) $ const 0
+  bounds = pad (Just 1) $ R.fromFunction (R.Z R.:. n R.:. n) $ const Nothing
+  ham = pad (0 :: Double) . R.map (*0.1) . lap
 
+cube :: Int -> System R.U (R.Z R.:. Int R.:. Int R.:. Int) Double
+cube n = System iv bounds ham where
+  iv = R.fromFunction (R.Z R.:. (n + 2) R.:. (n + 2) R.:. (n + 2)) $ const 0
+  bounds = pad (Just 1) $ R.fromFunction (R.Z R.:. n R.:. n R.:. n) $ const Nothing
+  ham = pad (0 :: Double) . R.map (*0.1) . lap
 
-b2 = pad (Just 1) $ R.fromFunction (R.Z R.:. (8 :: Int) R.:. (8 :: Int)) $ const Nothing
-bdiff2 = boundStep b2 (R.map (*0.1) . lap')
-a2 = R.fromFunction (R.Z R.:. (10 :: Int) R.:. (10 :: Int)) $ const 0
-l2 = simulate bdiff2 a2
+weirdLine :: Int -> System R.U (R.Z R.:. Int) Double
+weirdLine n = System iv bounds ham where
+  System iv bounds _ = line n
+  ham = pad (0 :: Double) . R.map (\x -> x * x * 0.1) . lap
 
-
-main :: IO()
-main = putStrLn $ show (l1 !! 10)
-
-
-
-{-
--- example 
-
-b1 = listArray (0, 10) (Just 1 : replicate 9 Nothing ++ [Just 1])
-bdiff1 = boundStep b1 (fmap (*0.1) . lap1)
-a1 = 0 :. listArray (0, 10) (replicate 11 $ Just 0)
-l1 = map (map rmJ . toList) $ simulate bdiff1 a1
-    where rmJ Nothing = undefined
-          rmJ (Just n) = n
-
-
-
-b2 = listArray ((0,0), (5,5)) $ join $ [[e,e,e,e,e,e],
-                                        [e,o,o,o,o,e],
-                                        [e,o,o,o,o,e],
-                                        [e,o,o,o,o,e],
-                                        [e,o,o,o,o,e],
-                                        [e,e,e,e,e,e]]
-    where e = Just 1
-          o = Nothing
-bdiff2 = boundStep b2 (fmap (*0.1) . lap2)
-a2 = (0,0) :. listArray ((0,0), (5,5)) (repeat $ Just 0)
-l2 = map (fmap rmJ) $ simulate (boundStep b2 bdiff2) a2
-    where rmJ Nothing = undefined
-          rmJ (Just n) = n
-
- -}
+-- yes, this main function is ugly; I couldnt figure out how to separate it into two case statements because of the type polymorphism
+main :: IO ()
+main = do [par, its, shape, size] <- S.getArgs
+          case (par, shape) of ("par", "line") -> simulateTo (read its) (line $ read size) >>= print
+                               ("seq", "line") -> simulateToSeq (read its) (line $ read size) >>= print
+                               ("par", "square") -> simulateTo (read its) (square $ read size) >>= print
+                               ("seq", "square") -> simulateToSeq (read its) (square $ read size) >>= print
+                               ("par", "cube") -> simulateTo (read its) (cube $ read size) >>= print
+                               ("seq", "cube") -> simulateToSeq (read its) (cube $ read size) >>= print
+                               ("par", "weird") -> simulateTo (read its) (weirdLine $ read size) >>= print
+                               ("seq", "weird") -> simulateToSeq (read its) (cube $ read size) >>= print
